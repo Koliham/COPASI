efad50e2ccae3509502e031e2f3c88a6e1512efb
Koliham <zelakri@web.de>
2014-09-25 15:35:28 +0200

    Loading Bar 2.3
    Just with the files for QGraphicsItem

---
+++ copasi/qlayout/CQGaugeItem.h
@@ -0,0 +1,23 @@
+//
+//#ifndef CQGAUGEITEM_H
+//#define CQGAUGEITEM_H
+//#include <QtGui/QPainter>
+//#include <QtGui/QGraphicsScene>
+////#include <QtGui/QGraphicsItem>
+//#include <Qt\qgraphicsitem.h>
+//class CQGaugeItem : public QGraphicsItem
+//
+//{
+//	
+//public:
+//    CQGaugeItem();
+//    QRectF boundingRect() const;
+//    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+//
+//public slots:
+//    void dragLeaveEvent();
+//    void dragMoveEvent();
+//
+//};
+//
+//#endif // CQGAUGEITEM_H
